73b15f0454f9db4611ecdfb652b9bc085abe55ab
Bayezid Mostafa <hiperx1@gmail.com>
2022-11-24 17:54:31 +0600

    Category API Created

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -6,7 +8,22 @@ const port = process.env.PORT || 5000
 // Middleware
 app.use(cors());
 app.use(express.json())
+const uri = process.env.URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const run = async () => {
+    try {
+        const categoryCollection = client.db('motoDreamDB').collection('category')
+        app.get('/category', async (req, res) => {
+            const query = {};
+            const category = await categoryCollection.find(query).toArray();
+            res.send(category);
+        })
+    }
+    catch { }
+    finally { }
+}
+run().catch(err => console.error(err));
 app.get('/', (req, res) => {
     res.send('Server is running')
 })
