93605a7d5f95b24bb947bafcb3ca5393314568f7
Bayezid Mostafa <hiperx1@gmail.com>
2022-11-28 02:20:10 +0600

    Payment Method Implemented & Update Product Status

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
+const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000
 const jwt = require('jsonwebtoken')
-const app = express();
+
+const stripe = require("stripe")(process.env.SECRET_STRIPE_KEY);
 
 // Middleware
 app.use(cors());
@@ -37,6 +39,7 @@ const run = async () => {
         const categoryItemsCollection = client.db('motoDreamDB').collection('products')
         const usersCollection = client.db('motoDreamDB').collection('users')
         const bookedCollection = client.db('motoDreamDB').collection('bookedProducts')
+        const paidCollection = client.db('motoDreamDB').collection('paidProducts')
 
         // Admin Verifications
         const verifyAdmin = async (req, res, next) => {
@@ -92,11 +95,14 @@ const run = async () => {
             const products = await categoryItemsCollection.find(query).toArray()
             res.send(products);
         })
-        // app.post('/category', async (req, res) => {
-        //     const user = req.body;
-        //     const result = await categoryItemsCollection.insertOne(user);
-        //     res.send(result);
-        // })
+        app.get('/myProducts/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = {
+                seller_email: email
+            }
+            const products = await categoryItemsCollection.find(query).toArray()
+            res.send(products);
+        })
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollection.insertOne(user);
@@ -105,6 +111,18 @@ const run = async () => {
         app.post('/bookedProducts', async (req, res) => {
             const product = req.body;
             const result = await bookedCollection.insertOne(product);
+            res.send(result);
+        })
+        app.get('/myorders/:email', async (req, res) => {
+            const email = req.params.email
+            const filter = { email: email }
+            const result = await bookedCollection.find(filter).toArray();
+            res.send(result)
+        })
+        app.get('/myorder/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await bookedCollection.findOne(filter);
             res.send(result);
         })
         app.get('/users', async (req, res) => {
@@ -117,9 +135,83 @@ const run = async () => {
             const user = await usersCollection.findOne({ email });
             res.send(user)
         })
-        app.post('/addProduct', async(req, res) => {
+        app.post('/addProduct', async (req, res) => {
             const product = req.body;
             const result = await categoryItemsCollection.insertOne(product);
+            res.send(result);
+        })
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ admin: user?.role === 'admin' });
+        })
+        app.get('/users/seller/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ seller: user?.role === 'seller' });
+        })
+        app.put('/advertisement/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    advertisement: true
+                }
+            }
+            const result = await categoryItemsCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
+        app.get('/advertised', async (req, res) => {
+            const filter = {
+                advertisement: true
+            }
+            const result = await categoryItemsCollection.find(filter).toArray();
+            res.send(result)
+        })
+        app.post('/create-payment-intent', async (req, res) => {
+            const order = req.body;
+            const price = order.price;
+            const amount = price * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+        app.post('/payments', async (req, res) => {
+            const paymentData = req.body;
+            const result = await paidCollection.insertOne(paymentData);
+            const id = paymentData.orderId;
+            const filter = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionID: paymentData.transactionID
+                }
+            }
+            const updateResult = await bookedCollection.updateOne(filter, updatedDoc);
+            res.send(updateResult)
+        })
+        app.put('/updateproductstatus/:bookingId', async (req, res) => {
+            const bookingId = req.params.bookingId;
+            const filter = { _id: ObjectId(bookingId) };
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: {
+                    product_status: 'sold',
+                    advertisement: false
+                }
+            }
+            const result = await categoryItemsCollection.updateOne(filter, updatedDoc, options)
             res.send(result);
         })
     }
